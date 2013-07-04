306ade41f940a1ea8a6ee0434a0bb427f907397c
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2013-07-04 21:09:10 +0200

    Polish translation.

---
+++ lang/pl.php
@@ -0,0 +1,74 @@
+<?php
+
+$menu = <<<EOD
+  <li><a href="#share">Rozgłoś tę odezwę</a></li>
+  <li><a href="https://www.change.org/petitions/eu-parliament-and-eu-commission-stop-mass-surveillance-protect-our-data-and-our-whistleblowers">Podpisz się pod petycją</a></li>
+  <!--li><a href="#">About the Pirate movement</a></li-->
+EOD;
+
+$share = "Rozgłoś tę odezwę wśród swoich znajomych... oraz NSA!";
+
+$manifesto = <<<EOD
+  <div class="first box">
+    <p><span class="intro">Jesteśmy zbulwersowani</span> odkryciem bezprecedensowej <strong>inwigilacji użytkowników Internetu</strong> na całym świecie za pomocą PRISM i innych podobnych programów. Stosowanie inwigilacji na taką skalę, bez żadnej kontroli społecznej, stanowi poważne <strong>zagrożenie dla praw człowieka, przede wszystkim dla wolności słowa i prawa do prywatności, a wraz z nimi dla fundamentów naszej demokracji</strong>.<br /><br /></p>
+    <p><span class="intro">Popieramy działania Edwarda Snowdena</span> na rzecz informowania opinii publicznej. Gdy rząd jest stanowiony przez obywateli i dla obywateli, to publiczne informowanie o przebiegu i zakresie działań takiego rządu nie może być traktowane jako przestępstwo przeciwko jego bezpieczeństwu. Działania demokratycznego rządu powinny opierać się na zgodzie jego obywateli. Nie istnieje taka zgoda, gdy obywatele nie są w pełni informowani o działaniach rządu.</p>
+    <p>Dostrzegamy z niepokojem całkowity brak poszanowania przez rząd USA praw obywateli europejskich oraz każdego, kto korzysta z amerykańskich usług telekomunikacyjnych i infrastruktury. Dostrzegamy również negatywne efekty jakie wywiera na swoich sojusznikach, na suwerenności krajów objętych inwigilacją oraz na konkurencyjności firm tych krajów.</p>
+    <p><span class="intro">Europa musi we właściwy sposób zareagować</span> na te rewelacje. W świetle tej sytuacji ważne jest, aby Unia Europejska, zamiast godzić się na uwikłanie w te nieakceptowalne dla społeczeństw nadużycia władzy, stała się <strong>światowym liderem praw cyfrowych i ochrony prywatności, przejrzystości rządzenia i ochrony demaskatorów nieetycznych działań</strong>.<br /><br /></p>
+    <p><span class="intro">Domagamy się:</span></p>
+  </div>            
+  <div class="box">
+    <h3><a href="#" onclick="return toggle(this)">1. Udzielenia azylu i ochrony dla demaskatorów</a></h3>
+    <div class="expanded">
+      <p>Rząd USA wykazał – w przypadku <a href="http://iam.bradleymanning.org">Bradleya Manninga</a> i innych – że <a href="http://www.guardian.co.uk/world/2012/mar/12/bradley-manning-cruel-inhuman-treatment-un">sposób traktowania przez niego demaskatorów</a> jest powodem do poważnego niepokoju. Poprzez oficjalne określanie Snowdena jako „zdrajcę“ przez różnych urzędników amerykańskich oraz media, został utworzony klimat, w którym nie można liczyć na sprawiedliwy proces. Istnieje obawa, że Snowden może być prześladowany z powodu swojego przekonania, że działania rządu powinny być przejrzyste i stoi teraz w obliczu ryzyka nieludzkiego lub poniżającego traktowania oraz groźby kary, w tym kary śmierci.</p>
+      <p>Wzywamy wszystkie rządy europejskie, aby <strong>przychyliły się do wniosku o polityczny azyl i udzielenie statusu uchodźcy</strong> dla pana Edwarda Snowdena oraz innych demaskatorów nieetycznych działań władzy.</p>
+    </div>
+  </div>
+  <div class="box">
+    <h3><a href="#" onclick="return toggle(this)">2. Ustalenia faktów</a></h3>
+    <div class="expanded">
+      <p>Jest niedopuszczalne, że tajne systemy inwigilacji <strong>powstają poza kontrolą demokratycznych instytucji</strong> oraz bez konsultacji społecznych.</p>
+      <p>Wzywamy Parlament Europejski do <strong>utworzenia komisji śledczej</strong>, zgodnie z <a href="http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-//EP//TEXT+RULES-EP+20130521+RULE-185+DOC+XML+V0//EN&language=EN&navigationBar=YES">art. 185 jego regulaminu</a>. Fakty, które należy ustalić, to:</p>
+      <ul class="chunk">
+        <li>Jakie są prawdziwe funkcje programu PRISM?</li>
+        <li>Z jakich danych i jakich źródeł korzysta?</li>
+        <li>Które organy administracyjne w UE i jej państw członkowskich miały wiedzę na temat PRISM oraz dostęp do tego i podobnych programów lub danych z nich uzyskanych?</li>
+        <li>W jakim stopniu zostały naruszone przepisy <a href="http://www.europarl.europa.eu/charter/default_en.htm">Karty Praw Podstawowych</a>, <a href="https://en.wikipedia.org/wiki/Data_Protection_Directive">dyrektywy o ochronie danych</a>, <a href="https://en.wikipedia.org/wiki/Directive_on_Privacy_and_Electronic_Communications">dyrektywy o prywatności i łączności elektronicznej</a>, lub innych przepisów UE?</li>
+      </ul>
+      <p>Poszerzamy to wezwanie do wszystkich parlamentów krajowych – w celu zbadania, czy zostały naruszone krajowe konstytucje oraz przepisy dotyczące szpiegostwa i ochrony danych.</p>
+    </div>
+  </div>
+  <div class="box supporters">
+    <h3><a href="#" onclick="return toggle(this)">3. Silnej europejskiej ochrony danych</a></h3>
+    <div class="expanded">
+      <p><a href="http://www.privacycampaign.eu/key-issues-of-the-regulation/">Muszą zostać wzmocnione</a> regulacje ogólnej ochrony danych (General Data Protection Regulation), aby zapewnić szeroką i dalekosiężną ochronę danych prywatnych i biznesowych. Należy postawić <a href="http://lobbyplag.eu/">opór lobbingowi</a>, który tę ochronę chce ograniczyć.</p>
+      <p>W szczególności, <strong>dane obywateli europejskich nie mogą być świadomie przekazywane do amerykańskich agencji wywiadowczych</strong>. Musi zostać przywrócony <a href="https://www.openrightsgroup.org/blog/2013/how-the-eu-commission-caved-to-us-demands-to-water-down-its-privacy-law">artykuł 42</a> z pierwszej propozycji projektu, który identyfikował inicjatywy państw trzecich, takie jak USA Patriot Act oraz USA Foreign Intelligence Surveillance Act, i stawiał barierę zagranicznym organom sądowym dla dostępu do europejskich danych. Metadane i dane anonimowe muszą także podlegać ochronie.</p>
+      <p>Według <a href="https://en.wikipedia.org/wiki/International_Safe_Harbor_Privacy_Principles">międzynarodowych zasad ochrony prywatności „Safe Harbor”</a>, amerykańskie firmy muszą poinformować użytkowników o udzieleniu dostępu do swoich danych podmiotom trzecim. Wydaje się, że firmy uczestniczące w programie PRISM naruszyły te przepisy. W odpowiedzi, UE <strong>musi odwołać zgodę</strong> na powyższe zasady (<a href="http://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CELEX:32000D0520:EN:HTML">decyzja Komisji 2000/520/WE</a>), a przedsiębiorstwa podlegające sądom europejskim powinny zaprzestać tych praktyk natychmiast. Safe Harbor musi być renegocjowane z bardziej skutecznymi zabezpieczeniami i wprowadzonymi mechanizmami odwoływania się lub zastąpione nowym międzynarodowym porozumieniem w sprawie ochrony danych, np. opartym na zasadach ogólnego rozporządzenia o ochronie danych.</p>
+    </div>
+  </div>
+  <div class="box">
+    <h3><a href="#" onclick="return toggle(this)">4. Międzynarodowego traktatu o wolności w Internecie</a></h3>
+    <div class="expanded">
+      <p>Aby zapewnić, że Internet pozostanie istotną siłą w procesie demokratyzacji społeczeństwa zamiast stać się narzędziem ograniczenia demokracji i indywidualnej wolności, UE powinna przewodzić <strong>międzynarodowemu traktatowi o wolności w Internecie</strong>. Taka umowa powinna zdecydowanie chronić poufność komunikacji, wolności wypowiedzi i dostępu do informacji (w szczególności, gdy odnosi się to do Internetu), a także neutralność sieci.</p>
+    </div>
+  </div>
+  <div class="box">
+    <h3><a href="#" onclick="return toggle(this)">5. Funduszy na oprogramowanie chroniące prywatność</a></h3>
+    <div class="expanded">
+      <p>Jako dodatkową linię ochrony prywatności, konsumenci powinni mieć <strong>możliwość korzystania z oprogramowania i usług, które mocno chronią ich prywatność</strong>. Takie oprogramowanie może zapewniać anonimowość, szyfrowanie komunikacji end-to-end, architekturę peer-to-peer, przechowywanie danych u użytkownika, audytowalne, otwarte kody źródłowe i inne funkcje ochrony prywatności.</p>
+      <p>Doceniamy fakt, że obecny wniosek dotyczący programu ramowego <a href="http://ec.europa.eu/research/horizon2020/index_en.cfm">„Horyzont 2020”</a> obejmuje cel <em><a href="http://www.kuleuven.be/eu/horizon2020/societal_challenges/security-more.html">„zapewnienie prywatności i wolności w Internecie”</a></em>. Wzywamy Unię Europejską do zapewnienia, że <strong>znacznie większy</strong> odsetek funduszy na badania będzie przeznaczany na oprogramowanie i usługi zwiększające prywatność obywateli, a nie na projekty mające cel przeciwny, tj. na projekty związane z gromadzeniem i przetwarzaniem danych o obywatelach, a <a href="https://en.wikipedia.org/wiki/INDECT">projekty, których wyraźnym celem jest masowa inwigilacja</a>, będą odrzucane.</p>
+    </div>          
+  </div>
+  <div class="box">
+    <h3><a href="#" onclick="return toggle(this)">6. Zapobieżenia powstaniu europejskiemu PRISM</a></h3>
+    <div class="expanded">
+      <p>Proponujemy, aby <strong>wprowadzić środki ustawodawcze w celu wzmocnienia obrony przed podobnymi programami w Europie</strong>.</p>
+      <p>Należy wyraźnie zakazać praktyk wpinania się w sieć internetową z <strong><a href="https://en.wikipedia.org/wiki/Tempora">podsłuchami</a></strong> tworzonymi przez agencje rządowe, takie jak NSA w ramach <a href="http://thinkprogress.org/justice/2013/06/10/2133201/blarney-online-surveillance/?mobile=nc">programu BLARNEY</a>. Takie podsłuchy umożliwiają przechowywanie i przeszukiwanie wszelkiej komunikacji internetowej, z pominięciem kontroli i procedur, powodując utratę poufności danych i prywatności wszystkich użytkowników sieci. Wiąże się to z naruszeniem integralności infrastruktury sieciowej, co w niedopuszczalny sposób podważa zaufanie do całego Internetu i zagraża wszystkim jego użytkownikom.</p>
+      <p>Ponawiamy apel o <strong>uchylenie <a href="https://en.wikipedia.org/wiki/Data_Retention_Directive">dyrektywy o retencji danych</a></strong>. Czeskie i rumuńskie sądy konstytucyjne wyraźnie underlinestwierdziłyunderline, że szeroka i nienadzorowana retencja danych jest naruszeniem podstawowych praw człowieka. Ustanawiając masowe gromadzenie dużych ilości danych bez zgody sądu, programy retencji danych prowadzą do nadużyć takich jak PRISM, zagrażając podziałowi władzy wykonawczej i sądowniczej, który to podział jest podstawą naszych demokracji.</p>
+    </div>          
+  </div>
+EOD;
+
+$undersigned = "Sygnatariusze";
+
+
+?>
